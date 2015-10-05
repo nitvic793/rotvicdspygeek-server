420096f90586d25edf18e1edb65611a88bd84222
Nitish Victor <nithishvictor@gmail.com>
2015-10-05 07:36:33 +0530

    Change deployment command

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side logic for managing auths
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-
+ 
 module.exports = {
   index: function (req, res) {
     var email = req.param('email');
