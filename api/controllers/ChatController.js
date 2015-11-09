837819e5617ef993cd636f86238ce8ed3336c8fd
Nitish Victor <nithishvictor@gmail.com>
2015-11-09 21:51:51 +0530

    Added push to server

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -4,6 +4,11 @@
  * @description :: Server-side logic for managing chats
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var ionicPushServer = require('ionic-push-server');
+var credentials = {
+    IonicApplicationID : "75748798",
+    IonicApplicationAPIsecret : "d6592183b787f804ee0fad1710967014adbe7fc02d9968e0"
+};
 
 module.exports = {
 	sayHi: function(req, res) {
@@ -15,5 +20,41 @@ module.exports = {
 	},
 	'get /__getcookie': function(req, res, next){
     res.send('_sailsIoJSConnect();');
+	},
+	createChat: function(req,res){
+
+		console.log(req.body);
+		Chat.create(req.body).exec(function(err,data){
+			var pushToken;
+			if(data.sender=='Teacher'){
+				Parents.findOne({'id':req.body.parent}).exec(function(err,data){
+					if(data.pushToken){
+						var notification = {
+							"tokens":[data.pushToken._token],
+							"notification":{
+								"title": "Message from " + data.firstname,
+								"alert":req.body.message
+								}
+						};
+						ionicPushServer(credentials, notification);
+					}
+				});
+			}
+			else{
+				Teachers.findOne({'id':req.body.teacher}).exec(function(err,data){
+					if(data.pushToken){
+						var notification = {
+							"tokens":[data.pushToken._token],
+							"notification":{
+								"title": "Message from " + data.firstname,
+								"alert":req.body.message
+								}
+						};
+						ionicPushServer(credentials, notification);
+					}
+				});
+			}
+			res.json(data);
+		});
 	}
 };
