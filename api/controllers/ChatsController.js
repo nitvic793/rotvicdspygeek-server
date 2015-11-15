8d3b6bb0948227037568991701d60c51e86e51d3
Nitish Victor <nithishvictor@gmail.com>
2015-11-15 16:14:47 +0530

    Chat now uses Sockets!

--- api/controllers/ChatsController.js
+++ api/controllers/ChatsController.js
@@ -9,14 +9,22 @@
      IonicApplicationID : "75748798",
      IonicApplicationAPIsecret : "d6592183b787f804ee0fad1710967014adbe7fc02d9968e0"
  };
+
+var sockets = []; //HACK!
+
 module.exports = {
 	createChat: function(req,res){
-
-		console.log(req.body);
+    function sendSocketMessage(userId){
+      console.log("Sending to Socket: ",userId);
+      if(sockets[userId]){
+        sails.sockets.emit(sockets[userId],"message",{"status":"New Message"});
+      }
+    }
 		Chats.create(req.body).exec(function(err,data){
 			var pushToken;
 			if(data.sender=='Teacher'){
 				Parents.findOne({'id':req.body.parent}).exec(function(err,data){
+          sendSocketMessage(req.body.parent);
 					if(data.pushToken){
 						var notification = {
 							"tokens":[data.pushToken],
@@ -31,6 +39,7 @@ module.exports = {
 			}
 			else{
 				Teachers.findOne({'id':req.body.teacher}).exec(function(err,data){
+          sendSocketMessage(req.body.teacher);
 					if(data.pushToken){
 						var notification = {
 							"tokens":[data.pushToken._token],
@@ -45,5 +54,20 @@ module.exports = {
 			}
 			res.json(data);
 		});
-	}
+	},
+  getSocketID: function(req, res) {
+  if (!req.isSocket) return res.badRequest();
+  var socketId = sails.sockets.id(req.socket);
+  return res.json({socketId:socketId});
+  },
+  registerSocket: function(req,res){
+    if(req.body.userId && req.body.socketId){
+      sockets[req.body.userId] = req.body.socketId;
+      res.json({"status":"registered"});
+      console.log(sockets);
+    }
+    else {
+      res.badRequest();
+    }
+  }
 };
